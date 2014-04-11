3cf84c78b8fb50d840ead0890395e25eb44e94bd
ChopsNappleSauce <nozzywuzhere@gmail.com>
2014-04-11 01:39:01 -0400

    ZombiePlain Example Uploaded
    This has a scene with the basic hex implementation in it.

---
+++ ZombiePlain/Assets/Cube Assets/Cube.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") > 0) {
+			transform.Translate (moveSpeed * Time.deltaTime, 0f, 0f);
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			transform.Translate (-moveSpeed * Time.deltaTime, 0f, 0f);
+		}
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			transform.Translate (0f, 0f, moveSpeed * Time.deltaTime);
+		} else if (Input.GetAxis ("Vertical") < 0) {
+			transform.Translate (0f, 0f, -moveSpeed * Time.deltaTime);
+		}
+	}
+}
