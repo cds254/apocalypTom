987af2d271c31b9abf90c21e5a4770219f0b6652
ChopsNappleSauce <nozzywuzhere@gmail.com>
2014-04-11 13:19:09 -0400

    Some minor script changes

--- ZombiePlain/Assets/Hex Assets/GridManager.cs
+++ ZombiePlain/Assets/Hex Assets/GridManager.cs
@@ -11,17 +11,28 @@ public class GridManager: MonoBehaviour
 	//next two variables can also be instantiated using unity editor
 	public int gridWidthInHexes = 10;
 	public int gridHeightInHexes = 10;
-	
+	public int dropDepth = 5;
+	public int preDropPenalty = 5;
+	public int dropDepthPenalty = 50;
+	public int postDropPenalty = 10;
 	//Hexagon tile width and height in game world
 	private float hexWidth;
 	private float hexHeight;
-	
+	private int currType;
+	private bool[,] occArray;
 	//Method to initialise Hexagon width and height
 	void setSizes()
 	{
 		//renderer component attached to the Hex prefab is used to get the current width and height
 		hexWidth = Hex1.renderer.bounds.size.x;
 		hexHeight = Hex1.renderer.bounds.size.z;
+	}
+
+	//initialize array to keep track which tiles have already been generated
+	void initOccArray()
+	{
+		occArray = new bool[gridWidthInHexes, gridHeightInHexes];
+		//occArray = Enumerable.Repeat(1,25).To2DArray(5,5);
 	}
 	
 	//Method to calculate the position of the first hexagon tile
@@ -80,6 +91,9 @@ public class GridManager: MonoBehaviour
 						hex = (GameObject)Instantiate(Hex1);
 						break;
 				}
+
+				//mark position as generated
+
 				//Current position in grid
 				Vector2 gridPos = new Vector2(x, y);
 				hex.transform.position = calcWorldCoord(gridPos);
@@ -92,6 +106,7 @@ public class GridManager: MonoBehaviour
 	void Start()
 	{
 		setSizes();
+		initOccArray();
 		createGrid();
 	}
 }
