3cfd37bc0a857b6282a8bc52dddf4f24e5e9b9d1
ChopsNappleSauce <nozzywuzhere@gmail.com>
2014-04-15 18:19:23 -0400

    Basic humanoid model added to project
    X and Z to levitate lulz

---
+++ ZombiePlain/Assets/Player Assets/Tom.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tom : MonoBehaviour {
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Horizontal") > 0) {
+			transform.Translate (-moveSpeed * Time.deltaTime, 0f, 0f);
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			transform.Translate (moveSpeed * Time.deltaTime, 0f, 0f);
+		}
+
+		if (Input.GetAxis ("Vertical") > 0) {
+			transform.Translate (0f, moveSpeed * Time.deltaTime, 0f);
+		} else if (Input.GetAxis ("Vertical") < 0) {
+			transform.Translate (0f, -moveSpeed * Time.deltaTime, 0f);
+		}
+
+		if (Input.GetKey (KeyCode.X)) {
+			transform.Translate (0f, 0f, moveSpeed * Time.deltaTime);
+		} else if (Input.GetKey (KeyCode.Z)) {
+			transform.Translate (0f, 0f, -moveSpeed * Time.deltaTime);
+		}
+	}
+}
