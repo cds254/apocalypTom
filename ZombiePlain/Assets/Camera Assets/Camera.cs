dbab26b6ab632c2db56feaaba4f76e1ce5ccad3b
ChopsNappleSauce <nozzywuzhere@gmail.com>
2014-04-16 00:00:55 -0400

    Mouse guided rotation added
    The way it is now was kind of an accident but I kinda like it

---
+++ ZombiePlain/Assets/Camera Assets/Camera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+
+	public float xOffset;
+	public float yOffset;
+	public float zOffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*
+		GameObject tom = GameObject.FindGameObjectWithTag ("Player");
+		Vector3 tomPos = tom.transform.position;
+		Debug.Log (tomPos.ToString());
+		tomPos.Set (tomPos.x + xOffset, tomPos.y + yOffset, tomPos.z + zOffset);
+		transform.position = tomPos;
+		*/
+
+		float angle = Mathf.Atan2(Input.mousePosition.y - Screen.height/2, Input.mousePosition.x - Screen.width/2) * Mathf.Rad2Deg;
+		Vector3 currAngles = transform.eulerAngles;
+		Quaternion target = Quaternion.Euler(currAngles.x, 180f+angle, currAngles.z);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 15f);
+	}
+}
--- ZombiePlain/Assets/Player Assets/Tom.cs
+++ ZombiePlain/Assets/Player Assets/Tom.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Tom : MonoBehaviour {
 
-	public float moveSpeed;
+	public float moveSpeed = 10;
+	public float rotateSpeed = 10;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,7 @@ public class Tom : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*
 		if (Input.GetAxis ("Horizontal") > 0) {
 			transform.Translate (-moveSpeed * Time.deltaTime, 0f, 0f);
 		} else if (Input.GetAxis ("Horizontal") < 0) {
@@ -23,11 +25,29 @@ public class Tom : MonoBehaviour {
 		} else if (Input.GetAxis ("Vertical") < 0) {
 			transform.Translate (0f, -moveSpeed * Time.deltaTime, 0f);
 		}
+		*/
+
+		if (Input.GetAxis ("Horizontal") > 0) {
+			transform.Translate (0f, -moveSpeed * Time.deltaTime, 0f);
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			transform.Translate (0f, moveSpeed * Time.deltaTime, 0f);
+		}
+		
+		if (Input.GetAxis ("Vertical") > 0) {
+			transform.Translate (-moveSpeed * Time.deltaTime, 0f, 0f);
+		} else if (Input.GetAxis ("Vertical") < 0) {
+			transform.Translate (moveSpeed * Time.deltaTime, 0f, 0f);
+		}
 
 		if (Input.GetKey (KeyCode.X)) {
 			transform.Translate (0f, 0f, moveSpeed * Time.deltaTime);
 		} else if (Input.GetKey (KeyCode.Z)) {
 			transform.Translate (0f, 0f, -moveSpeed * Time.deltaTime);
 		}
+
+		float angle = Mathf.Atan2(Input.mousePosition.y - Screen.height/2, Input.mousePosition.x - Screen.width/2) * Mathf.Rad2Deg;
+		Vector3 currAngles = transform.eulerAngles;
+		Quaternion target = Quaternion.Euler(currAngles.x, 180f-angle, currAngles.z);
+		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 15f);
 	}
 }
