ac553f6c86b4c8923d9d294a6c5e45ce20fee2b5
ChopsNappleSauce <nozzywuzhere@gmail.com>
2014-04-16 13:55:37 -0400

    Camera Fixed
    Tom movement needs to be changed still

--- ZombiePlain/Assets/Camera Assets/Camera.cs
+++ ZombiePlain/Assets/Camera Assets/Camera.cs
@@ -7,9 +7,15 @@ public class Camera : MonoBehaviour {
 	public float yOffset;
 	public float zOffset;
 
+	public float moveSpeed = 10;
+	public float rotateSpeed = 10;
+
+	private Vector3 startPoint;
+
 	// Use this for initialization
 	void Start () {
-	
+		startPoint = transform.position;
+		startPoint += new Vector3 (-xOffset, -yOffset, -zOffset);
 	}
 	
 	// Update is called once per frame
@@ -22,9 +28,25 @@ public class Camera : MonoBehaviour {
 		transform.position = tomPos;
 		*/
 
+		/*
 		float angle = Mathf.Atan2(Input.mousePosition.y - Screen.height/2, Input.mousePosition.x - Screen.width/2) * Mathf.Rad2Deg;
 		Vector3 currAngles = transform.eulerAngles;
 		Quaternion target = Quaternion.Euler(currAngles.x, 180f+angle, currAngles.z);
 		transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * 15f);
+		*/
+
+		if (Input.GetAxis ("Horizontal") > 0) {
+			startPoint += new Vector3 (moveSpeed * Time.deltaTime, 0f, 0f);
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			startPoint += new Vector3 (-moveSpeed * Time.deltaTime, 0f, 0f);
+		}
+		
+		if (Input.GetAxis ("Vertical") > 0) {
+			startPoint += new Vector3 (0f, 0f, moveSpeed * Time.deltaTime);
+		} else if (Input.GetAxis ("Vertical") < 0) {
+			startPoint += new Vector3 (0f, 0f, -moveSpeed * Time.deltaTime);
+		}
+
+		transform.position = startPoint + new Vector3(xOffset, yOffset, zOffset);
 	}
 }
