a08007a7eb85e0085116c325f8e9079131fca683
ChopsNappleSauce <nozzywuzhere@gmail.com>
2014-04-23 21:30:21 -0400

    Some code refactoring and commenting
    Some minor code fixes. Still more refactoring and commenting to do

--- ZombiePlain/Assets/Hex Assets/GridManager.cs
+++ ZombiePlain/Assets/Hex Assets/GridManager.cs
@@ -3,35 +3,38 @@ using System.Collections;
 
 public class GridManager: MonoBehaviour
 {
-	//following public variable is used to store the hex model prefab;
-	//instantiate it by dragging the prefab on this variable using unity editor
-	public GameObject Hex1;
-	public GameObject Hex2;
-	public GameObject Hex3;
-	public GameObject BlankHex;
-	//next two variables can also be instantiated using unity editor
+	//Hex prefab GameObjects
+	public GameObject Hex1;	    //Plains
+	public GameObject Hex2;     //Forest
+	public GameObject Hex3;     //Desert
+	public GameObject BlankHex; //Unthemed
+
+	//Public variables
 	public int gridWidthInHexes = 10;
 	public int gridHeightInHexes = 10;
-	public int dropDepth = 5;
-	public int preDropPenalty = 5;
-	public int dropDepthPenalty = 50;
-	public int postDropPenalty = 10;
-	public int biomeCores = 15;
-	//Hexagon tile width and height in game world
+	public int biomeCores = 15;      //Determines the complexity of the ground layout
+	public int minCoreSpacing = 10;  //Min spacing between cores in x and y direction
+	public int spacingRetries = 50;  //Max attempts to generate more evenly spaced cores
+	public int maxTypeDisparity = 2; //Max disparity amongst core types
+	public int typeRetries = 50;     //Max attempts to generate more evenly numbered core types
+
+	//Private global variables
 	private float hexWidth;
 	private float hexHeight;
 	private int currType;
-	private bool[,] occArray;
-	private Vector3[] coreArray;
+	private bool[,] occArray;    //Keeps track of which tiles have been placed
+	private Vector3[] coreArray; //Keeps track of each core's coords and hex type
+
 	//Method to initialise Hexagon width and height
 	void setSizes()
 	{
-		//renderer component attached to the Hex prefab is used to get the current width and height
+		//Height and width determined by Plains tile
+		//Assumed that tiles are the same size
 		hexWidth = Hex1.renderer.bounds.size.x;
 		hexHeight = Hex1.renderer.bounds.size.z;
 	}
 
-	//initialize array to keep track which tiles have already been generated
+	//Initialize arrays to keep track which tiles have already been generated
 	void initArrays()
 	{
 		//Default value is false for each bool
@@ -51,7 +54,7 @@ public class GridManager: MonoBehaviour
 		return initPos;
 	}
 	
-	//method used to convert hex grid coordinates to game world coordinates
+	//Method used to convert hex grid coordinates to game world coordinates
 	public Vector3 calcWorldCoord(Vector2 gridPos)
 	{
 		//Position of the first hex tile
@@ -66,56 +69,73 @@ public class GridManager: MonoBehaviour
 		float z = initPos.z - gridPos.y * hexHeight * 0.75f;
 		return new Vector3(x, 0, z);
 	}
-	
-	//Finally the method which initialises and positions all the tiles
-	void createGrid()
-	{
-		//Game object which is the parent of all the hex tiles
-		GameObject hexGridGO = new GameObject("HexGrid");
 
-		//Randomly generate core tiles
+	/**************************************************************************************
+	 * This method loops, generating core locations in coreArray, until the amount of cores
+	 * denoted by the public variable biomeCores all have their locations assigned.
+	 * 
+	 * For each new core, a random position is generated within the bounds of the grid, as
+	 * set by the public variables, gridWidthInHexes and gridHeightInHexes.
+	 * 
+	 * Once a location is generated, the method loops through all of the previously 
+	 * generated cores to see if the new core is within the minimum spacing between core
+	 * (specified by the public variable, minCoreSpacing) in both the x and y(z) directions.
+	 * 
+	 * If the new core is too close to another core, it is regenerated and rechecked.
+	 * This process is repeated until either a location far enough from the other cores is
+	 * generated or the max retry limit (denoted by the public variable, spacingRetries) is
+	 * reached.
+	 * ************************************************************************************/
+	void generateCoreLocations()
+	{
+		//Loop until all cores are generated
 		for (int i = 0; i < biomeCores; i++) 
 		{
+			//Generate a random location for this core
 			coreArray [i] = new Vector3 ((float)Random.Range (0, gridWidthInHexes), 
 			                             (float)Random.Range (0, gridHeightInHexes), 0f);
-
+			
 			//Make sure it's not right next to another one
 			bool redo = false;
-			int minSpace = 10;
 			for(int x = 0; x < i; x++)
 			{
-				if((Mathf.Abs(coreArray[x].x - coreArray[i].x) < minSpace) &&
-				   (Mathf.Abs(coreArray[x].x - coreArray[i].x) < minSpace))
+				if((Mathf.Abs(coreArray[x].x - coreArray[i].x) < minCoreSpacing) &&
+				   (Mathf.Abs(coreArray[x].y - coreArray[i].y) < minCoreSpacing))
 					redo = true;
 			}
+
+			//If too close, keep retrying
 			int j = 0; //counter to make sure there is not an infinite loop
-			while((j < 50) && redo)
+			while((j < spacingRetries) && redo)
 			{
 				coreArray [i] = new Vector3 ((float)Random.Range (0, gridWidthInHexes), 
 				                             (float)Random.Range (0, gridHeightInHexes), 0f);
-
+				
 				for(int x = 0; x < i; x++)
 				{
-					if((Mathf.Abs(coreArray[x].x - coreArray[i].x) < minSpace) &&
-					   (Mathf.Abs(coreArray[x].x - coreArray[i].x) < minSpace))
+					if((Mathf.Abs(coreArray[x].x - coreArray[i].x) < minCoreSpacing) &&
+					   (Mathf.Abs(coreArray[x].y - coreArray[i].y) < minCoreSpacing))
 						redo = true;
 					else
 						redo = false;
 				}
-
+				
 				j++;
 			}
 		}
+	}
 
+	void setCoreTypes()
+	{
+		//Counters for each core type
 		int plainCount = 0;
 		int forestCount = 0;
 		int desertCount = 0;
+
 		for (int i = 0; i < biomeCores; i++)
 		{
 			int hexType;
 			bool redo = false;
-			int offNum = 2;
-			int maxRedo = 10;
 			int j = 0;
 			do
 			{
@@ -124,57 +144,67 @@ public class GridManager: MonoBehaviour
 				switch(hexType)
 				{
 				case 0:
-					if(((plainCount - forestCount) >= offNum) &&
-					   ((plainCount - desertCount) >= offNum))
+					if(((plainCount - forestCount) >= maxTypeDisparity) &&
+					   ((plainCount - desertCount) >= maxTypeDisparity))
 						redo = true;
 					break;
 				case 1:
-					if(((forestCount - plainCount) >= offNum) &&
-					   ((forestCount - desertCount) >= offNum))
+					if(((forestCount - plainCount) >= maxTypeDisparity) &&
+					   ((forestCount - desertCount) >= maxTypeDisparity))
 						redo = true;
 					break;
 				case 2:
-					if(((desertCount - forestCount) >= offNum) &&
-					   ((desertCount - plainCount) >= offNum))
+					if(((desertCount - forestCount) >= maxTypeDisparity) &&
+					   ((desertCount - plainCount) >= maxTypeDisparity))
 						redo = true;
 					break;
 				default:
 					break;
 				}
-
-			}while(redo && (j <= maxRedo));
-
+				
+			}while(redo && (j <= typeRetries));
+			
 			GameObject coreHex = null;
 			switch(hexType)
 			{
-				case 0:
-					coreHex = (GameObject)Instantiate(Hex1);
-					coreArray[i].z = 1f;
-					plainCount++;
-					break;
-				case 1:
-					coreHex = (GameObject)Instantiate(Hex2);
-					coreArray[i].z = 2f;
-					forestCount++;
-					break;
-				case 2:
-					coreHex = (GameObject)Instantiate(Hex3);
-					coreArray[i].z = 3f;
-					desertCount++;
-					break;
-				default:
-					coreHex = (GameObject)Instantiate(Hex1);
-					coreArray[i].z = 1f;
-					plainCount++;
-					break;
+			case 0:
+				coreHex = (GameObject)Instantiate(Hex1);
+				coreArray[i].z = 1f;
+				plainCount++;
+				break;
+			case 1:
+				coreHex = (GameObject)Instantiate(Hex2);
+				coreArray[i].z = 2f;
+				forestCount++;
+				break;
+			case 2:
+				coreHex = (GameObject)Instantiate(Hex3);
+				coreArray[i].z = 3f;
+				desertCount++;
+				break;
+			default:
+				coreHex = (GameObject)Instantiate(Hex1);
+				coreArray[i].z = 1f;
+				plainCount++;
+				break;
 			}
-
+			
+			//Set core hex's position to its corresponding world coordinate
 			coreHex.transform.position = calcWorldCoord(new Vector2(coreArray[i].x, coreArray[i].y));
-			coreHex.transform.parent = hexGridGO.transform;
-
+			
 			//Mark core tiles as occupied
 			occArray[(int)coreArray[i].x, (int)coreArray[i].y] = true;
 		}
+	}
+
+	//Method that initialises and positions all the tiles
+	void createGrid()
+	{
+		//Randomly generate core tile locations
+		generateCoreLocations ();
+
+		//Set type and place each core
+		setCoreTypes ();
 
 		bool[] expCores = new bool[biomeCores]; //which cores are expired
 		int expCount = 0; //count of expired cores
@@ -228,7 +258,7 @@ public class GridManager: MonoBehaviour
 
 						//Current position in grid
 						hex.transform.position = calcWorldCoord(currPos);
-						hex.transform.parent = hexGridGO.transform;
+						//hex.transform.parent = hexGridGO.transform;
 
 						occArray[(int)currPos.x, (int)currPos.y] = true;
 					}
@@ -407,7 +437,7 @@ public class GridManager: MonoBehaviour
 
 							//Current position in grid
 							hex.transform.position = calcWorldCoord(nextPos);
-							hex.transform.parent = hexGridGO.transform;
+							//hex.transform.parent = hexGridGO.transform;
 							
 							occArray[(int)nextPos.x, (int)nextPos.y] = true;
 
@@ -433,36 +463,6 @@ public class GridManager: MonoBehaviour
 		{
 			for (int x = 0; x < gridWidthInHexes; x++)
 			{
-				/*
-				int hexType = Random.Range(0, 3);
-				//GameObject assigned to Hex public variable is cloned
-				GameObject hex = null;
-				switch(hexType)
-				{
-					case 0:
-						hex = (GameObject)Instantiate(Hex1);
-						break;
-					case 1:
-						hex = (GameObject)Instantiate(Hex2);
-						break;
-					case 2:
-						hex = (GameObject)Instantiate(Hex3);
-						break;
-					default:
-						hex = (GameObject)Instantiate(Hex1);
-						break;
-				}
-				*/
-				bool isCore = false;
-				for (int i = 0; i < biomeCores; i++) 
-				{
-					Vector2 coreCoord = coreArray[i];
-					if((coreCoord.x == x) && (coreCoord.y == y))
-					{
-						isCore = true;
-					}
-				}
-
 				if(!occArray[x,y])
 				{
 					int hexType = Random.Range(0, 3);
@@ -487,7 +487,6 @@ public class GridManager: MonoBehaviour
 					//Current position in grid
 					Vector2 gridPos = new Vector2(x, y);
 					hex.transform.position = calcWorldCoord(gridPos);
-					hex.transform.parent = hexGridGO.transform;
 				}
 			}
 		}
